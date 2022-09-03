14d8323d7b85b7e77b9367a6a0a36dd1c44a0154
Clément de Louvencourt <c.delouvencourt@nocturne.app>
2022-09-03 10:42:04 +0200

    Add url for self hosted

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,14 @@
 set -e
 
 # Create new Sentry release
-sentry-cli releases new -p $SENTRY_PROJECT $RELEASE
-sentry-cli releases set-commits "$RELEASE" --commit "$GITHUB_PROJECT@$RELEASE"
+sentry-cli --url $SENTRY_URL releases new -p $SENTRY_PROJECT $RELEASE
+sentry-cli --url $SENTRY_URL releases set-commits "$RELEASE" --commit "$GITHUB_PROJECT@$RELEASE"
 
 # Create new deploy for this Sentry release
-sentry-cli releases deploys $RELEASE new -e $DEPLOY_ENV
+sentry-cli --url $SENTRY_URL releases deploys $RELEASE new -e $DEPLOY_ENV
 
 # Upload sourcemaps to Sentry
-sentry-cli releases files $RELEASE upload-sourcemaps --ext ts --ext js --ext map --url-prefix "$URL_PREFIX" --no-rewrite $SOURCEMAP_PATH
+sentry-cli --url $SENTRY_URL releases files $RELEASE upload-sourcemaps --ext ts --ext js --ext map --url-prefix "$URL_PREFIX" --no-rewrite $SOURCEMAP_PATH
 
 # Finalize the release
-sentry-cli releases finalize $RELEASE
+sentry-cli --url $SENTRY_URL releases finalize $RELEASE
